53cf5e6dd76d91ab66bb03e9d848109017f372c2
LOL808 <helloworld808@hotmail.com>
2017-08-22 23:26:11 +0800

    = =

--- Assets/Script/Lootable.cs
+++ Assets/Script/Lootable.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class Lootable : MonoBehaviour {
 	public GameObject item;
-	public List<GameObject> items;
+//	public List<GameObject> items;
+	private int _playerLayerMask;
 	// Use this for initialization
 	void Start () {
-		
+		_playerLayerMask = LayerMask.GetMask ("Player");
 	}
-	
+
+	void OnTriggerEnter(Collider other) {
+//		print (other.gameObject.layer);
+//		print (_playerLayerMask);
+		print (other.gameObject.tag);
+
+
+		if (other.gameObject.tag == "Player") {
+			GameObject leftHand = GameObject.Find ("mixamorig:LeftHand");
+//			GameObject leftHand = other.gameObject.transform.Find ("mixamorig:LeftHand").gameObject;	
+			Transform holdPos = GameObject.Find("handHoldPos").gameObject.transform;
+			item.transform.parent = leftHand.transform;
+//			item.transform = holdPos;
+			item.transform.position = holdPos.position;
+			item.transform.rotation = holdPos.rotation;
+			Destroy (gameObject);
+		}
+
+	}
 	// Update is called once per frame
 //	void Update () {
 //		item.transform.R
